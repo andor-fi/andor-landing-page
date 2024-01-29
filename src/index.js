0094e9c7060b70ed103954a2d5cc3ee29d5c0957
unknown <you@example.com>
2024-01-30 01:35:47 +0530

    removed extra files

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "@material-ui/styles";
-import { CssBaseline } from "@material-ui/core";
 import "../src/scss/main.css";
-import { LayoutProvider } from "./context/LayoutContext";
-import { UserProvider } from "./context/UserContext";
-import UserContext from "./context/User";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import MomentUtils from "@date-io/moment";
 import App from "./App";
 
 ReactDOM.render(
-      <UserContext>
-        <LayoutProvider>
-          <UserProvider>
-            <ThemeProvider >
-              <MuiPickersUtilsProvider utils={MomentUtils}>
-                <CssBaseline />
-                <App />
-              </MuiPickersUtilsProvider>
-
-              <ToastContainer />
-            </ThemeProvider>
-          </UserProvider>
-        </LayoutProvider>
-      </UserContext>,
+      <App />,
   document.getElementById("root")
 );
 
